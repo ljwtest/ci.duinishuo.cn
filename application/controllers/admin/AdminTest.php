3b32f2ce3fd062b3bd70d704bd1665fba6a297a1
ljw <654055148@qq.com>
2016-06-08 11:31:33 +0800

    1

---
+++ application/controllers/admin/AdminTest.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class AdminTest extends CI_Controller {
+
+	public function index()
+	{
+		echo "this is Admin test Controller";
+		$this->load->database();
+		//$query = $this->db->query('SELECT * from table2')->result();
+		//$query=$this->db->select("id,add_time")->get("table1")->result();
+		$arr=array("user"=>"ljw3","age"=>"26");
+		//echo config_item("mysql_host");
+		$query=$this->db->get("table1")->row_array();
+		//echo $query[0]['id'];die;
+		var_dump($query);
+		$this->load->view("test/index");
+	}
+}
